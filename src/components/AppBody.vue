30f185494eb381f21ff5f29e6386fb25f7ec69d9
King Fisher <tokingfisher19@gmail.com>
2019-04-26 16:12:12 +0600

    Vue Life cycle hocks

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -1,15 +1,52 @@
 <template>
-    <div id="body">
-
+    <div id="body" v-on:mousemove="getOrigin">
+        <span>{{ x }} X {{ y }}</span>
     </div>
 </template>
 
 <script>
     export default {
-        name: "AppBody"
+        name: "AppBody",
+        data(){
+            return{
+                x:0,
+                y:0,
+            }
+        },
+        beforeCreate() {
+            alert('before create')
+        },
+        created() {
+            alert('created');
+        },
+        beforeMount() {
+            alert('before Mounted')
+        },
+        mounted() {
+            alert('Mounted');
+        },
+        beforeUpdate() {
+            alert('before update');
+        },
+        updated() {
+
+            alert('updated');
+        },
+        methods:{
+            getOrigin(event){
+                this.x= event.offsetX;
+                this.y = event.offsetY;
+            }
+        }
+
     }
 </script>
 
 <style scoped>
-
+#body{
+    height: 300px;
+    width: 400px;
+    margin: 10px auto;
+    border: 2px solid red;
+}
 </style>
