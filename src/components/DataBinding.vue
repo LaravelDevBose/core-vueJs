efc5f100b154f2559e4412aa1b9896e1d47be01c
King Fisher <tokingfisher19@gmail.com>
2019-04-27 15:18:58 +0600

    check box binding

--- src/components/DataBinding.vue
+++ src/components/DataBinding.vue
@@ -10,7 +10,25 @@
                 <textarea  cols="30" rows="10" v-model="form.address"></textarea>
                 <p>Address: {{ form.address}}</p>
             </div>
+            <div class="py-5">
+                <label > CB One</label>
+                <input type="checkbox" value="1" v-model="form.checkBoxes">
+                <label > CB Two</label>
+                <input type="checkbox" value="2" v-model="form.checkBoxes">
+                <label > CB Three</label>
+                <input type="checkbox" value="3" v-model="form.checkBoxes">
+                <label > CB Four</label>
+                <input type="checkbox" value="4" v-model="form.checkBoxes">
+                <label > CB Five</label>
+                <input type="checkbox" value="5" v-model="form.checkBoxes">
+            </div>
 
+            <div>
+                <h3>Check Box Result</h3>
+                <ul>
+                    <li v-for="(check,index) in form.checkBoxes" :key="index">{{ check }}</li>
+                </ul>
+            </div>
         </form>
     </div>
 </template>
@@ -22,7 +40,8 @@
             return{
                 form:{
                     name:"",
-                    address:""
+                    address:"",
+                    checkBoxes:[],
                 }
             }
         }
